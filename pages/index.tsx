9ee665f00debbd7967444d439829f0a8de65792b
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-09-16 18:04:31 +1000

    Add stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,40 @@
-import * as React from 'react'
+import * as React from "react";
 
-import { ExtendedRecordMap } from 'notion-types'
+import { ExtendedRecordMap } from "notion-types";
 
-import * as notion from '../lib/notion'
-import { NotionPage } from '../components/NotionPage'
+import * as notion from "../lib/notion";
+import { NotionPage } from "../components/NotionPage";
 import {
   previewImagesEnabled,
   rootDomain,
-  rootNotionPageId
-} from '../lib/config'
+  rootNotionPageId,
+} from "../lib/config";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
-  const pageId = rootNotionPageId
-  const recordMap = await notion.getPage(pageId)
+  const pageId = rootNotionPageId;
+  const recordMap = await notion.getPage(pageId);
 
   return {
     props: {
-      recordMap
+      recordMap,
     },
-    revalidate: 10
-  }
-}
+    revalidate: 10,
+  };
+};
 
 export default function Page({ recordMap }: { recordMap: ExtendedRecordMap }) {
   return (
-    <NotionPage
-      recordMap={recordMap}
-      rootDomain={rootDomain}
-      rootPageId={rootNotionPageId}
-      previewImagesEnabled={previewImagesEnabled}
-    />
-  )
+    <>
+      <Head>
+        <title>{process.env.websiteTitle}</title>
+      </Head>
+      <NotionPage
+        recordMap={recordMap}
+        rootDomain={rootDomain}
+        rootPageId={rootNotionPageId}
+        previewImagesEnabled={previewImagesEnabled}
+      />
+    </>
+  );
 }
