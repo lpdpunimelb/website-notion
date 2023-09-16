a8b6af2ff889c7ea4a0dfe553cff0ff7a9f4d81b
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-09-16 18:36:14 +1000

    Disable preview image

--- lib/config.ts
+++ lib/config.ts
@@ -1,13 +1,13 @@
 // TODO: change these to your own values
 // NOTE: rootNotionSpaceId is optional; set it to undefined if you don't want to
 // use it.
-export const rootNotionPageId = 'fea00fbfd6354c33b11da5f995d5b995'
+export const rootNotionPageId = "fea00fbfd6354c33b11da5f995d5b995";
 export const rootNotionSpaceId = undefined; // 'fde5ac74-eea3-4527-8f00-4482710e1af3'
 
 // NOTE: having this enabled can be pretty expensive as it re-generates preview
 // images each time a page is built. In a production setting, we recommend that
 // you cache the preview image results in a key-value database.
-export const previewImagesEnabled = true
+export const previewImagesEnabled = false;
 
 // Whether to use the official public Notion API or the unofficial private API.
 // Note that the official API doesn't expose formatting options for many blocks
@@ -15,10 +15,10 @@ export const previewImagesEnabled = true
 // If you want to use the official API, you must provide a NOTION_TOKEN env var.
 export const useOfficialNotionAPI =
   false ||
-  (process.env.USE_OFFICIAL_NOTION_API === 'true' && process.env.NOTION_TOKEN)
+  (process.env.USE_OFFICIAL_NOTION_API === "true" && process.env.NOTION_TOKEN);
 
 export const isDev =
-  process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
+  process.env.NODE_ENV === "development" || !process.env.NODE_ENV;
 
-export const port = process.env.PORT || 3000
-export const rootDomain = isDev ? `localhost:${port}` : null
+export const port = process.env.PORT || 3000;
+export const rootDomain = isDev ? `localhost:${port}` : null;
